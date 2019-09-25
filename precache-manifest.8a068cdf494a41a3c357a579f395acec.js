6cd1c447e1e5c8049f2bf27b3ca7751849c4c1e0
Navdeep <10390511+sivunq@users.noreply.github.com>
2019-09-25 09:06:54 +0530

    Updates

---
+++ precache-manifest.8a068cdf494a41a3c357a579f395acec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d2edfdef627ee29666c431e7336d16",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "162c733c326c030e5164",
+    "url": "/myWebsite/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6a49bc1861c9fa45503c",
+    "url": "/myWebsite/static/js/2.6c229a7c.chunk.js"
+  },
+  {
+    "revision": "162c733c326c030e5164",
+    "url": "/myWebsite/static/js/main.aace81af.chunk.js"
+  },
+  {
+    "revision": "89f9d63733129e56ebec",
+    "url": "/myWebsite/static/js/runtime-main.8d9eb216.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myWebsite/static/media/logo.25bf045c.svg"
+  }
+]);
